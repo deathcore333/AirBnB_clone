7ed54ab080bed0c6b7a3cf5ebd5eba370c101404
root <amossmwangii@gmail.com>
2023-02-12 15:13:15 +0000

    a new basemodel added

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines a BaseModel class with all common attributes/
+methods for other classes"""
+import json
+import os
+import models
+from datetime import datetime
+
+
+class BaseModel:
+
