a73113482cd6a4fe03621dc0742e84083f241966
Amos Maina <amossmwangii@gmail.com>
2023-02-12 11:15:31 -0800

    Defining a file that handles input and output of JSON for storage of all class instances

---
+++ models/engine/file_storage.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Handles I/O, writing and reading, of JSON for storage of all class instances
+"""
+import json
+from models import base_model
+from datetime import datetime
+
+strptime = datetime.strptime
+to_json = base_model.BaseModel.to_json
+
+
+class FileStorage:
+    """
+        handles long term storage of all class instances
+    """
+    CNC = {
+        'BaseModel': base_model.BaseModel,
+        'Amenity': amenity.Amenity,
+        'City': city.City,
+        'Place': place.Place,
+        'Review': review.Review,
+        'State': state.State,
+        'User': user.User
+    }
+    """CNC - this variable is a dictionary with:
+    keys: Class Names
+    values: Class type (used for instantiation)
+    """
+    __file_path = './dev/file.json'
+    __object = {}
+
+    def all(self, cls=None):
+        """
+            return private attribute: __objects
+        """
+        if cls is not None:
+            new_objs = {}
+            for clsid, obj in FileStorage.__objects.items():
+                if type(obj).__name__ == cls:
+                    new_objs[clsid] = obj
+            return new_objs
+        else:
+            return FileStorage.__objects
+
+    def new(self, obj):
+        """
+            sets / updates in __objects the obj with key <obj class name>.id
+        """
+        bm_id = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[bm_id] = obj
+        
+    def save(self):
+        """
+            serializes __objects to the JSON file
+        """
+        fname = FileStorage.__file_path
+        storage_d = {}
+        for bm_id, bm_obj in FileStorage.__objects.items():
+            storage_d[bm_id] = bm_obj.to_json(saving_file_storage=True)
+        with open(fname, mode='w', encoding='utf-8') as f_io:
+            json.dump(storage_d, f_io)
