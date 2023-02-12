1b079bae54546c29c84df1ada089eb095deb0e98
Amos Maina <amossmwangii@gmail.com>
2023-02-12 10:20:26 -0800

    added some more tests for the BaseModel Class

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -29,4 +29,22 @@ class TestBaseModelDocs(unittest.TestCase):
         actual = models.base_model.__doc__
         self.assertEqual(expected, actual)
 
+    def test_all_function_docs(self):
+        """... tests for ALL DOCS for all funcions in db_storage file"""
+        all_functions = TestBaseModelDocs.all_funcs
+        for function in all_functions:
+            self.assertIsNotNone(function[1].__doc__)
+
+    def test_pep8_base_model(self):
+        """... base_model.py conforms to PEP8 Style"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        errors = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(errors.total_errors, 0, errors.messages)
+
+    def test_file_is_executable(self):
+        """... tests if file has correct permissions so user can execute"""
+        file_stat = stat('models/base_model.py')
+        permissions = str(oct(file_stat[0]))
+        actual = int(permissions[5:-2]) >= 5
+        self.assertTrue(actual)
 
